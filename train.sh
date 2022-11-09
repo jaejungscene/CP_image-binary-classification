35f5277cfbb77d344b0dbacbb8fc79c7fe1ece9a
jaejung <ljj0512@gmail.com>
2022-11-09 17:45:22 +0900

    delete 1000_test.zip

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python train.py --lr 1e-4 --epoch 50 --batch_size 32 --wandb 1 --cuda 0,1,6,7,8 --optimizer sgd  --scheduler cosinerestarts
+python train.py --model efficientnet_b6 --lr 1e-4 --epoch 50 --batch_size 32 --wandb 1 --cuda 0,1,6,7,8 --optimizer sgd  --scheduler cosinerestarts
